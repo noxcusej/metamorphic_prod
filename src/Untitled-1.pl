fb2ae47fdb4ba65f13dd204afd44fadad58a3cac
noxcusej <jgarbis@gmail.com>
2023-01-10 00:25:11 -0500

    Publish latest

---
+++ src/Untitled-1.pl
@@ -0,0 +1,141 @@
+
+
+ WARNING  Compiled with 1 warning                                                11:59:45 AM
+
+[eslint] 
+/Users/jordangarbis/Metamorphic_Deploy/atemporal2/src/babylon/ForVR.ts
+   4:5   warning  'Camera' is defined but never used                                @typescript-eslint/no-unused-vars
+   9:5   warning  'UniversalCamera' is defined but never used                       @typescript-eslint/no-unused-vars
+  11:5   warning  'ShadowLight' is defined but never used                           @typescript-eslint/no-unused-vars
+  13:5   warning  'PointLight' is defined but never used                            @typescript-eslint/no-unused-vars
+  14:5   warning  'SpotLight' is defined but never used                             @typescript-eslint/no-unused-vars
+  17:5   warning  'DynamicTexture' is defined but never used                        @typescript-eslint/no-unused-vars
+  20:5   warning  'AbstractMesh' is defined but never used                          @typescript-eslint/no-unused-vars
+  22:5   warning  'GlowLayer' is defined but never used                             @typescript-eslint/no-unused-vars
+  23:5   warning  'MeshAssetTask' is defined but never used                         @typescript-eslint/no-unused-vars
+  24:5   warning  'ScreenSpaceReflectionPostProcess' is defined but never used      @typescript-eslint/no-unused-vars
+  25:5   warning  'WebXRHitTest' is defined but never used                          @typescript-eslint/no-unused-vars
+  26:5   warning  'GizmoManager' is defined but never used                          @typescript-eslint/no-unused-vars
+  27:5   warning  'LightGizmo' is defined but never used                            @typescript-eslint/no-unused-vars
+  28:5   warning  'Light' is defined but never used                                 @typescript-eslint/no-unused-vars
+  29:5   warning  'ShadowGenerator' is defined but never used                       @typescript-eslint/no-unused-vars
+  32:5   warning  'CubeTexture' is defined but never used                           @typescript-eslint/no-unused-vars
+  33:5   warning  'Mesh' is defined but never used                                  @typescript-eslint/no-unused-vars
+  35:5   warning  'Material' is defined but never used                              @typescript-eslint/no-unused-vars
+  36:5   warning  'WebVRFreeCamera' is defined but never used                       @typescript-eslint/no-unused-vars
+  37:5   warning  'VRDeviceOrientationFreeCamera' is defined but never used         @typescript-eslint/no-unused-vars
+  38:5   warning  'KeyboardEventTypes' is defined but never used                    @typescript-eslint/no-unused-vars
+  40:5   warning  'TransformNode' is defined but never used                         @typescript-eslint/no-unused-vars
+  41:5   warning  'TrajectoryClassifier' is defined but never used                  @typescript-eslint/no-unused-vars
+  42:5   warning  'EasingFunction' is defined but never used                        @typescript-eslint/no-unused-vars
+  43:5   warning  'QuarticEase' is defined but never used                           @typescript-eslint/no-unused-vars
+  45:5   warning  'Animation' is defined but never used                             @typescript-eslint/no-unused-vars
+  46:5   warning  'ArcRotateCamera' is defined but never used                       @typescript-eslint/no-unused-vars
+  47:5   warning  'TouchCamera' is defined but never used                           @typescript-eslint/no-unused-vars
+  49:5   warning  'DeviceOrientationCamera' is defined but never used               @typescript-eslint/no-unused-vars
+  50:5   warning  'PhysicsImpostor' is defined but never used                       @typescript-eslint/no-unused-vars
+  51:5   warning  'CannonJSPlugin' is defined but never used                        @typescript-eslint/no-unused-vars
+  52:5   warning  'ActionManager' is defined but never used                         @typescript-eslint/no-unused-vars
+  53:5   warning  'SetValueAction' is defined but never used                        @typescript-eslint/no-unused-vars
+  54:5   warning  'ExecuteCodeAction' is defined but never used                     @typescript-eslint/no-unused-vars
+  55:5   warning  'Action' is defined but never used                                @typescript-eslint/no-unused-vars
+  56:5   warning  'PerfCounter' is defined but never used                           @typescript-eslint/no-unused-vars
+  57:5   warning  'StopSoundAction' is defined but never used                       @typescript-eslint/no-unused-vars
+  58:5   warning  'Sound' is defined but never used                                 @typescript-eslint/no-unused-vars
+  59:5   warning  'HDRCubeTexture' is defined but never used                        @typescript-eslint/no-unused-vars
+  60:5   warning  'VolumetricLightScatteringPostProcess' is defined but never used  @typescript-eslint/no-unused-vars
+  61:5   warning  'WebXRDefaultExperience' is defined but never used                @typescript-eslint/no-unused-vars
+  65:12  warning  'AdvancedDynamicTexture' is defined but never used                @typescript-eslint/no-unused-vars
+  65:36  warning  'Button' is defined but never used                                @typescript-eslint/no-unused-vars
+  65:44  warning  'GUI3DManager' is defined but never used                          @typescript-eslint/no-unused-vars
+  65:58  warning  'HolographicButton' is defined but never used                     @typescript-eslint/no-unused-vars
+  65:84  warning  'StackPanel' is defined but never used                            @typescript-eslint/no-unused-vars
+  77:29  warning  Unexpected any. Specify a different type                          @typescript-eslint/no-explicit-any
+  78:17  warning  Unexpected any. Specify a different type                          @typescript-eslint/no-explicit-any
+
+/Users/jordangarbis/Metamorphic_Deploy/atemporal2/src/babylon/MetaRoom4.ts
+    4:3   warning  'Camera' is defined but never used                                @typescript-eslint/no-unused-vars
+    9:3   warning  'UniversalCamera' is defined but never used                       @typescript-eslint/no-unused-vars
+   11:3   warning  'ShadowLight' is defined but never used                           @typescript-eslint/no-unused-vars
+   13:3   warning  'PointLight' is defined but never used                            @typescript-eslint/no-unused-vars
+   14:3   warning  'SpotLight' is defined but never used                             @typescript-eslint/no-unused-vars
+   17:3   warning  'DynamicTexture' is defined but never used                        @typescript-eslint/no-unused-vars
+   20:3   warning  'AbstractMesh' is defined but never used                          @typescript-eslint/no-unused-vars
+   23:3   warning  'MeshAssetTask' is defined but never used                         @typescript-eslint/no-unused-vars
+   24:3   warning  'ScreenSpaceReflectionPostProcess' is defined but never used      @typescript-eslint/no-unused-vars
+   25:3   warning  'WebXRHitTest' is defined but never used                          @typescript-eslint/no-unused-vars
+   26:3   warning  'GizmoManager' is defined but never used                          @typescript-eslint/no-unused-vars
+   27:3   warning  'LightGizmo' is defined but never used                            @typescript-eslint/no-unused-vars
+   28:3   warning  'Light' is defined but never used                                 @typescript-eslint/no-unused-vars
+   29:3   warning  'ShadowGenerator' is defined but never used                       @typescript-eslint/no-unused-vars
+   32:3   warning  'CubeTexture' is defined but never used                           @typescript-eslint/no-unused-vars
+   33:3   warning  'Mesh' is defined but never used                                  @typescript-eslint/no-unused-vars
+   35:3   warning  'Material' is defined but never used                              @typescript-eslint/no-unused-vars
+   36:3   warning  'WebVRFreeCamera' is defined but never used                       @typescript-eslint/no-unused-vars
+   37:3   warning  'VRDeviceOrientationFreeCamera' is defined but never used         @typescript-eslint/no-unused-vars
+   38:3   warning  'KeyboardEventTypes' is defined but never used                    @typescript-eslint/no-unused-vars
+   40:3   warning  'TransformNode' is defined but never used                         @typescript-eslint/no-unused-vars
+   41:3   warning  'TrajectoryClassifier' is defined but never used                  @typescript-eslint/no-unused-vars
+   42:3   warning  'EasingFunction' is defined but never used                        @typescript-eslint/no-unused-vars
+   43:3   warning  'QuarticEase' is defined but never used                           @typescript-eslint/no-unused-vars
+   44:3   warning  'DefaultRenderingPipeline' is defined but never used              @typescript-eslint/no-unused-vars
+   45:3   warning  'Animation' is defined but never used                             @typescript-eslint/no-unused-vars
+   46:3   warning  'ArcRotateCamera' is defined but never used                       @typescript-eslint/no-unused-vars
+   47:3   warning  'TouchCamera' is defined but never used                           @typescript-eslint/no-unused-vars
+   49:3   warning  'DeviceOrientationCamera' is defined but never used               @typescript-eslint/no-unused-vars
+   50:3   warning  'PhysicsImpostor' is defined but never used                       @typescript-eslint/no-unused-vars
+   51:3   warning  'CannonJSPlugin' is defined but never used                        @typescript-eslint/no-unused-vars
+   53:3   warning  'SetValueAction' is defined but never used                        @typescript-eslint/no-unused-vars
+   55:3   warning  'Action' is defined but never used                                @typescript-eslint/no-unused-vars
+   56:3   warning  'PerfCounter' is defined but never used                           @typescript-eslint/no-unused-vars
+   57:3   warning  'StopSoundAction' is defined but never used                       @typescript-eslint/no-unused-vars
+   59:3   warning  'HDRCubeTexture' is defined but never used                        @typescript-eslint/no-unused-vars
+   60:3   warning  'VolumetricLightScatteringPostProcess' is defined but never used  @typescript-eslint/no-unused-vars
+   64:34  warning  'Button' is defined but never used                                @typescript-eslint/no-unused-vars
+   64:42  warning  'GUI3DManager' is defined but never used                          @typescript-eslint/no-unused-vars
+   64:56  warning  'HolographicButton' is defined but never used                     @typescript-eslint/no-unused-vars
+   64:82  warning  'StackPanel' is defined but never used                            @typescript-eslint/no-unused-vars
+   76:27  warning  Unexpected any. Specify a different type                          @typescript-eslint/no-explicit-any
+   77:15  warning  Unexpected any. Specify a different type                          @typescript-eslint/no-explicit-any
+  628:9   warning  'modelll' is assigned a value but never used                      @typescript-eslint/no-unused-vars
+
+/Users/jordangarbis/Metamorphic_Deploy/atemporal2/src/components/BabylonMetaRoom.vue
+  22:20  warning  Forbidden non-null assertion  @typescript-eslint/no-non-null-assertion
+
+/Users/jordangarbis/Metamorphic_Deploy/atemporal2/src/components/Choice.vue
+  25:20  warning  Unexpected any. Specify a different type  @typescript-eslint/no-explicit-any
+
+/Users/jordangarbis/Metamorphic_Deploy/atemporal2/src/components/ForVR.vue
+  22:20  warning  Forbidden non-null assertion  @typescript-eslint/no-non-null-assertion
+
+✖ 95 problems (0 errors, 95 warnings)
+
+
+You may use special comments to disable some warnings.
+Use // eslint-disable-next-line to ignore the next line.
+Use /* eslint-disable */ to ignore all warnings in a file.
+
+  App running at:
+  - Local:   http://localhost:8080/ 
+  - Network: http://10.0.0.12:8080/
+
+ERROR in src/components/Choice.vue:27:14
+TS2339: Property '$router' does not exist on type '{ choose(choice: any): void; }'.
+    25 |     choose(choice: any){
+    26 |       if(choice === 'oculus'){
+  > 27 |         this.$router.push({ path: '/oculus' });
+       |              ^^^^^^^
+    28 |       } else {
+    29 |         this.$router.push({ path: '/web' })
+    30 |       }
+
+ERROR in src/components/Choice.vue:29:14
+TS2339: Property '$router' does not exist on type '{ choose(choice: any): void; }'.
+    27 |         this.$router.push({ path: '/oculus' });
+    28 |       } else {
+  > 29 |         this.$router.push({ path: '/web' })
+       |              ^^^^^^^
+    30 |       }
+    31 |     }
+    32 |   }
+
